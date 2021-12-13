b1998325d7e348ac206998083be449172474c821
axpbth <axpbth@gmail.com>
2021-12-13 09:41:21 -0600

    Style FAQ

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Signup from '../src/components/Signup/Signup';
 import Profile from './pages/Profile';
 import Signin from "../src/components/Signin/Signin";
 import ForgotPassword from '../src/components/Signin/ForgotPassword/ForgotPassword';
+import FAQ from './pages/FAQ.jsx';
 
 function App() {
     const [isOpen, setIsOpen] = useState(false);
@@ -26,6 +27,7 @@ function App() {
         <Route path="forgotpassword" element={<ForgotPassword />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/aboutus" element={<Aboutus />} />
+        <Route path="/faq" element={<FAQ />}/>
       </Routes>
     </Router>
   );
