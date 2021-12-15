4a1ef1e33571732d996c8be99bd811570ccec39f
axpbth <axpbth@gmail.com>
2021-12-15 10:06:19 -0600

    Amogus, i cant change the font size by itself :(

--- src/components/GeneralPurpose/GPElements.jsx
+++ src/components/GeneralPurpose/GPElements.jsx
@@ -8,38 +8,38 @@ export const Bg = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  overflow: hidden;   
+  overflow: hidden;
 `;
 
 export const BgImage = styled.img`
-    background-color: black;
-    filter: blur(2px);
-    width: 100%;
-    height: 100%;
-    -o-object-fit: cover;
-    object-fit: cover;
+  background-color: black;
+  filter: blur(2px);
+  width: 100%;
+  height: 100%;
+  -o-object-fit: cover;
+  object-fit: cover;
 `;
 
 export const TextCont = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 export const TextP = styled.p`
-    color: #fff;
+  color: #fff;
+  font-size: 16px;
+  text-align: center;
+  text-shadow: 4px 4px 25px #000000;
+  font-weight: 400;
+
+  @media screen and (max-width: 768px) {
     font-size: 16px;
-    text-align: center;
-    text-shadow: 4px 4px 25px #000000;
-    font-weight: 400;
+  }
 
-    @media screen and (max-width: 768px) {
-        font-size: 16px;
-    }
-
-    @media screen and (max-width: 480px) {
-        font-size: 16px;
-    }
+  @media screen and (max-width: 480px) {
+    font-size: 16px;
+  }
 `;
 
 export const TextH1 = styled.h1`
@@ -71,12 +71,12 @@ export const TextH2 = styled.h2`
 
   @media screen and (max-width: 480px) {
     font-size: 30px;
-  }    
+  }
 `;
 
 export const TextH3 = styled.h3`
   color: #fff;
-  font-size: 30px;
+  font-size: 40px; /*${({ big }) => (big ? "50px" : "30px")};*/
   text-align: center;
   text-shadow: 4px 4px 25px #000000;
   font-weight: 400;
