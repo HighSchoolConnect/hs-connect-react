7c7b98b1da22c764bfaeed43a3b040177184abcf
axpbth <axpbth@gmail.com>
2022-02-11 09:50:17 -0600

    Amogus

---
+++ src/pages/ProfileCreation.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProfileCreationHero from "../components/ProfileCreation/ProfileCreationHero";
+
+const ProfileCreation = () => {
+  return (
+    <>
+      <ProfileCreation />
+    </>
+  );
+};
+
+export default ProfileCreation;
