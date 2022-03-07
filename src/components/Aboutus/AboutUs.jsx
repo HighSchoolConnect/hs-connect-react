a09e51b8b603bc613aea9040a8347aa4fa66f185
axpbth <axpbth@gmail.com>
2022-03-07 10:08:44 -0600

    Fixed my last name

--- src/components/Aboutus/AboutUs.jsx
+++ src/components/Aboutus/AboutUs.jsx
@@ -24,28 +24,21 @@ const AboutUs = () => {
           <TextH2>About HS Connect</TextH2>
           <TextH3>What is our Vision?</TextH3>
           <TextP>
-            Create opportunities for students to find internships and jobs that will help them 
-            exel in their future careers.
+            Create opportunities for students to find internships and jobs that
+            will help them exel in their future careers.
           </TextP>
           <TextH3>Who We Are</TextH3>
           <TextP>
-            HS Connect began with four seniors that had a dream to provide every 
-            highschool sudent with an internship. What was first a small project has now grown into a nationwide buisness and has helped countless
+            HS Connect began with four seniors that had a dream to provide every
+            highschool sudent with an internship. What was first a small project
+            has now grown into a nationwide buisness and has helped countless
             students gain experience in fields they are passionate in.
           </TextP>
           <TextH3>Meet our Team</TextH3>
-          <TextTeam>
-            Geeth Gunnampalli - Full Stack Developer 
-          </TextTeam>
-          <TextTeam>
-            Adarsh Pullasari - Full Stack Developer
-          </TextTeam>
-          <TextTeam>
-           Gana Ventrapragada - Front End Developer
-          </TextTeam>
-          <TextTeam>
-            Kamalesh Mohanasundar - Front End Developer
-          </TextTeam>
+          <TextTeam>Geeth Gunnampalli - Full Stack Developer</TextTeam>
+          <TextTeam>Adarsh Pulasseri - Full Stack Developer</TextTeam>
+          <TextTeam>Gana Ventrapragada - Front End Developer</TextTeam>
+          <TextTeam>Kamalesh Mohanasundar - Front End Developer</TextTeam>
         </TextCont>
       </AboutUsContent>
     </AboutUsContainer>
