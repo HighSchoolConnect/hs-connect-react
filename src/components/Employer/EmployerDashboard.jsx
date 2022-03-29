7776b82822b4959628870f092977b70f7cc228a6
Geeth S Gunnampalli <geeth.gunnampalli@gmail.com>
2022-03-29 14:18:36 -0500

    Remove Unused Popover Elements

--- src/components/Employer/EmployerDashboard.jsx
+++ src/components/Employer/EmployerDashboard.jsx
@@ -32,10 +32,8 @@ import {
   PopoverContent,
   PopoverHeader,
   PopoverBody,
-  PopoverFooter,
   PopoverArrow,
   PopoverCloseButton,
-  PopoverAnchor,
 } from "@chakra-ui/react";
 import {
   AttachmentIcon,
