0f13fd2546525b6ec05f527abf1831eb0ea1b31f
Geeth S Gunnampalli <geeth.gunnampalli@gmail.com>
2022-04-01 09:17:52 -0500

    Add Animated Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import React, { useState, lazy, Suspense } from "react";
 import { Flex, Spinner } from "@chakra-ui/react";
 // import Home from "./pages/Home.jsx";
@@ -18,29 +18,10 @@ import { Flex, Spinner } from "@chakra-ui/react";
 // import Resume from "./components/Resume/Resume";
 // import ApplicationPage from "./components/ApplicationPage/ApplicationPage";
 // import ResumeEmployer from "./components/Resume/ResumeEmployer";
-
-const Home = lazy(() => import("./pages/Home.jsx"));
-const Aboutus = lazy(() => import("./pages/Aboutus.jsx"));
 const Navbar = lazy(() => import("./components/Navbar/Navbar"));
 const Sidebar = lazy(() => import("./components/Sidebar/Sidebar"));
 const Footer = lazy(() => import("./components/Footer/Footer"));
-const Profile = lazy(() => import("./pages/Profile.jsx"));
-const FAQ = lazy(() => import("./pages/FAQ.jsx"));
-const Results = lazy(() => import("./pages/Results"));
-const Signup = lazy(() => import("./components/Signup/Signup"));
-const Signin = lazy(() => import("./components/Signin/Signin"));
-const ForgotPassword = lazy(() =>
-  import("./components/Signin/ForgotPassword/ForgotPassword")
-);
-const Contact = lazy(() => import("./components/Contact/Contact"));
-const EmployerDashboard = lazy(() =>
-  import("./components/Employer/EmployerDashboard")
-);
-const Resume = lazy(() => import("./components/Resume/Resume"));
-const ResumeEmployer = lazy(() => import("./components/Resume/ResumeEmployer"));
-const ApplicationPage = lazy(() =>
-  import("./components/ApplicationPage/ApplicationPage")
-);
+const PageRoutes = lazy(() => import("./PageRoutes"));
 const renderLoader = () => (
   <Flex justifyContent="center" alignItems="center" height="100vh" bg="#000">
     <Spinner size="xl" thickness="4px" speed="0.65s" emptyColor="gray.200" />
@@ -57,23 +38,7 @@ function App() {
       <Router>
         <Sidebar isOpen={isOpen} toggle={toggle} />
         <Navbar toggle={toggle} />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/aboutus" element={<Aboutus />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/faq" element={<FAQ />} />
-          <Route path="/results" element={<Results />} />
-          <Route path="/results/:id" element={<Results />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/forgotpassword" element={<ForgotPassword />} />
-          <Route path="/contactus" element={<Contact />} />
-          <Route path="/dashboard" element={<EmployerDashboard />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="/resume/:id" element={<ResumeEmployer />} />
-          <Route path="/apply" element={<ApplicationPage />} />
-          <Route path="/apply/:id/:title" element={<ApplicationPage />} />
-        </Routes>
+        <PageRoutes />
         <Footer />
       </Router>
     </Suspense>
---
+++ src/PageRoutes.jsx
@@ -0,0 +1,51 @@
+import React, { lazy } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+
+const Home = lazy(() => import("./pages/Home.jsx"));
+const Aboutus = lazy(() => import("./pages/Aboutus.jsx"));
+const Profile = lazy(() => import("./pages/Profile.jsx"));
+const FAQ = lazy(() => import("./pages/FAQ.jsx"));
+const Results = lazy(() => import("./pages/Results"));
+const Signup = lazy(() => import("./components/Signup/Signup"));
+const Signin = lazy(() => import("./components/Signin/Signin"));
+const ForgotPassword = lazy(() =>
+  import("./components/Signin/ForgotPassword/ForgotPassword")
+);
+const Contact = lazy(() => import("./components/Contact/Contact"));
+const EmployerDashboard = lazy(() =>
+  import("./components/Employer/EmployerDashboard")
+);
+const Resume = lazy(() => import("./components/Resume/Resume"));
+const ResumeEmployer = lazy(() => import("./components/Resume/ResumeEmployer"));
+const ApplicationPage = lazy(() =>
+  import("./components/ApplicationPage/ApplicationPage")
+);
+
+
+const PageRoutes = () => {
+  const location = useLocation();
+  return (
+    <AnimatePresence>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="/aboutus" element={<Aboutus />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/results/:id" element={<Results />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path="/contactus" element={<Contact />} />
+        <Route path="/dashboard" element={<EmployerDashboard />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/resume/:id" element={<ResumeEmployer />} />
+        <Route path="/apply" element={<ApplicationPage />} />
+        <Route path="/apply/:id/:title" element={<ApplicationPage />} />
+      </Routes>
+    </AnimatePresence>
+  );
+};
+
+export default PageRoutes;
